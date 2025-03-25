b8da006c5388cdf03dd1c54345d0edea41ae5202
Janus Developer <dev@retrievo.net>
2025-03-25 09:34:36 -0400

    add buttons for interactivity with embedded chart

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-const btnHide = document.getElementById('btn-hide');
-const btnShow = document.getElementById('btn-show');
-const btnPdf = document.getElementById('btn-pdf');
-const btnImg = document.getElementById('btn-img');
 const containerDiv = document.getElementById('viz-container');
 const url =
     'https://public.tableau.com/views/LearnEmbeddedAnalytics/SalesOverviewDashboard';
@@ -18,8 +14,30 @@ function initViz() {
     viz = new tableau.Viz(containerDiv, url, options);
 }
 
+function getRangeValues() {
+    const minValue = document.getElementById('min-value').value;
+    const maxValue = document.getElementById('max-value').value;
+
+    const workbook = viz.getWorkbook();
+
+    // get the active sheet in the window - this is the dashboard
+    const activeSheet = workbook.getActiveSheet();
+    const sheets = activeSheet.getWorksheets();
+    const chart = sheets[1];
+
+    chart.applyRangeFilterAsync('Sales', { min: minValue, max: maxValue }).then(
+        () => console.log('Filter applied!'),
+        (error) => console.error(`Failed to apply filter: ${error}`)
+    );
+}
+
 document.addEventListener('DOMContentLoaded', initViz);
-btnHide.addEventListener('click', () => viz.hide());
-btnShow.addEventListener('click', () => viz.show());
-btnPdf.addEventListener('click', () => viz.showExportPDFDialog());
-btnImg.addEventListener('click', () => viz.showExportImageDialog());
+document.getElementById('btn-hide').addEventListener('click', () => viz.hide());
+document.getElementById('btn-show').addEventListener('click', () => viz.show());
+document
+    .getElementById('btn-pdf')
+    .addEventListener('click', () => viz.showExportPDFDialog());
+document
+    .getElementById('btn-img')
+    .addEventListener('click', () => viz.showExportImageDialog());
+document.getElementById('btn-filter').addEventListener('click', getRangeValues);
